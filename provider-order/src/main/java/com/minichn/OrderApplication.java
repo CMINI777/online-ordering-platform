9579bf6a3fa2e0931e71ad346334c0a88c581d6a
CMINI777 <18254669984@163.com>
2019-08-27 00:34:27 +0800

    update order

--- provider-order/src/main/java/com/minichn/OrderApplication.java
+++ provider-order/src/main/java/com/minichn/OrderApplication.java
@@ -1,5 +1,6 @@
 package com.minichn;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by minichn on 2019/8/22 0022.
  */
 @SpringBootApplication
+@MapperScan("com.minichn.repository")
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class,args);
